a74875e28be28730cc4a1a7baa9778348e9db634
pdemp1729 <pdempster2@gmail.com>
2020-09-22 20:35:11 +0100

    Add test suite for Farey algorithm functionality

---
+++ farey/tests/__init__.py
@@ -0,0 +1,10 @@
+from farey.utils import almost_equal
+
+
+def assert_almost_equal(a, b, places=7, message=None):
+    """
+    Fail if numbers `a` and `b` are not close enough
+    """
+    if message is None:
+        message = f"{a} !~= {b} to {places} places"
+    assert almost_equal(a, b, places), message
