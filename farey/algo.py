9a175d278bdfec0931e4989edec43fec03303bb2
pdemp1729 <pdempster2@gmail.com>
2020-09-25 12:25:31 +0100

    Update Farey algorithm
    - The right-most limit can be improved by noting that we always
    begin the Farey algorithm by running through 1/1, 1/2, 1/3, ...
    until we get to the largest n for which 1/n is still greater than
    x. Therefore we might as well start with this 1/n as the right-most
    limit.
    - For numbers between 0.5 and 1 we can find the approximation to
    1-x, and use that. This enables us to use the above trick for
    numbers close to 1.

--- farey/algo.py
+++ farey/algo.py
@@ -1,3 +1,5 @@
+import math
+
 from farey.data import Rational
 from farey.utils import almost_equal
 
@@ -20,9 +22,15 @@ def find_rational_approximation(x, places=7):
         return -find_rational_approximation(-x, places)
     elif x >= 1:
         return int(x // 1) + find_rational_approximation(x % 1, places)
+    elif 0.5 < x < 1:
+        return 1 - find_rational_approximation(1 - x, places)
+
+    # we only need to look in the range between (0, 1)
+    # and (1, n) for n = math.floor(1 / x), since we will always reach
+    # this eventually by the Farey addition
 
     left = Rational(0, 1)
-    right = Rational(1, 1)
+    right = Rational(1, math.floor(1 / x))
     if almost_equal(x, left, places):
         return left
     elif almost_equal(x, right, places):
