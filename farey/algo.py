cd7af31dde4a2f63a78635de0d12f12220f2428a
pdemp1729 <pdempster2@gmail.com>
2020-09-26 20:38:44 +0100

    Extend API for find_rational_approximation function
    - We now allow the user to select a maximum size for the
    denominator, instead of a desired accuracy. The related Farey
    algorithm is similar to before.
    - Extend the test suite to ensure 100% coverage according to
    pytest-cov.

--- farey/algo.py
+++ farey/algo.py
@@ -9,7 +9,7 @@ def farey_add(x: Rational, y: Rational) -> Rational:
     return Rational(x.numerator + y.numerator, x.denominator + y.denominator)
 
 
-def find_rational_approximation(x, places=7):
+def find_rational_approximation(x, method="farey", places=None, max_denominator=None):
     """Find a rational approximation of x to the specified number of decimal places.
 
     We use an algorithm based on the Farey sequence, which consists of all completely
@@ -19,15 +19,35 @@ def find_rational_approximation(x, places=7):
     if x == 0:
         return Rational(0, 1)
     elif x < 0:
-        return -find_rational_approximation(-x, places)
+        return -find_rational_approximation(-x, method, places, max_denominator)
     elif x >= 1:
-        return int(x // 1) + find_rational_approximation(x % 1, places)
+        return int(x // 1) + find_rational_approximation(
+            x % 1, method, places, max_denominator
+        )
     elif 0.5 < x < 1:
-        return 1 - find_rational_approximation(1 - x, places)
+        return 1 - find_rational_approximation(1 - x, method, places, max_denominator)
 
-    # we only need to look in the range between (0, 1)
-    # and (1, n) for n = math.floor(1 / x), since we will always reach
-    # this eventually by the Farey addition
+    if method == "farey":
+        if places is not None and max_denominator is None:
+            return _farey_algorithm_accuracy(x, places)
+        if places is None and max_denominator is not None:
+            return _farey_algorithm_denominator(x, max_denominator)
+        else:
+            print("Must specify one of decimal places or maximum denominator")
+            return None
+
+
+def _farey_algorithm_accuracy(x, places=7):
+    """Find a rational approximation of x to the specified number of decimal places.
+
+    We use an algorithm based on the Farey sequence, which consists of all completely
+    reduced fractions between 0 and 1. This ensures that the rational approximation
+    found by the algorithm is completely reduced.
+
+    To speed up the process for small x, note that we only need to look in the range between
+    (0, 1) and (1, N), for N = math.floor(1 / x), since we will always reach this region
+    eventually by repeated Farey addition.
+    """
 
     left = Rational(0, 1)
     right = Rational(1, math.floor(1 / x))
@@ -48,3 +68,39 @@ def find_rational_approximation(x, places=7):
             return mediant
 
     return mediant
+
+
+def _farey_algorithm_denominator(x, max_denominator=1000):
+    """Find a rational approximation of x with denominator no larger than that specified.
+
+    We use an algorithm based on the Farey sequence, which consists of all completely
+    reduced fractions between 0 and 1. This ensures that the rational approximation
+    found by the algorithm is completely reduced.
+
+    To speed up the process for small x, note that we only need to look in the range between
+    (0, 1) and (1, N), for N = math.floor(1 / x), since we will always reach this region
+    eventually by repeated Farey addition.
+    """
+
+    N = math.floor(1 / x)
+    if max_denominator <= N:
+        return min(
+            [Rational(0, 1), Rational(1, max_denominator)], key=lambda r: abs(x - r)
+        )
+
+    left = Rational(0, 1)
+    right = Rational(1, N)
+
+    while max(left.denominator, right.denominator) < max_denominator:
+        mediant = farey_add(left, right)
+        if mediant.denominator > max_denominator:
+            # the current bounds are as good as we can do, so have to choose the best of them
+            break
+        if x < mediant:
+            right = mediant
+        elif x > mediant:
+            left = mediant
+        else:
+            return mediant
+
+    return min([left, right], key=lambda r: abs(x - r))
--- farey/tests/test_algo.py
+++ farey/tests/test_algo.py
@@ -5,29 +5,87 @@ from farey.data import Rational
 from farey.tests import assert_almost_equal
 
 
+def test_input():
+    x = 0.5
+    r = find_rational_approximation(x, method="farey")
+    assert r is None
+
+
 def test_rational_approximation_of_irrational():
-    x = math.sqrt(2) - 1
-    r = find_rational_approximation(x, places=7)
+    x = math.sqrt(2) - 1  # 0.41421356237309515
+    r = find_rational_approximation(x, method="farey", places=7)
     assert r == Rational(2378, 5741)
     assert_almost_equal(x, float(r), places=7)
+
+    r = find_rational_approximation(x, method="farey", max_denominator=1000)
+    assert r == Rational(408, 985)
 
 
 def test_rational_approximation_of_rational():
     x = 0.5
-    r = find_rational_approximation(x, places=7)
+    r = find_rational_approximation(x, method="farey", places=7)
     assert r == Rational(1, 2)
     assert_almost_equal(x, float(r), places=7)
 
+    x = 0.4
+    r = find_rational_approximation(x, method="farey", places=7)
+    assert r == Rational(2, 5)
+    assert_almost_equal(x, float(r), places=7)
+
+    x = 0.5
+    r = find_rational_approximation(x, method="farey", max_denominator=1000)
+    assert r == Rational(1, 2)
+
+    x = 0.4
+    r = find_rational_approximation(x, method="farey", max_denominator=1000)
+    assert r == Rational(2, 5)
+
+
+def test_rational_approximation_zero():
+    x = 0
+    r = find_rational_approximation(x, method="farey", places=7)
+    assert r == Rational(0, 1)
+
 
 def test_rational_approximation_less_than_zero():
-    x = 1 - math.sqrt(3)
-    r = find_rational_approximation(x, places=7)
+    x = 1 - math.sqrt(3)  # -0.7320508075688772
+    r = find_rational_approximation(x, method="farey", places=7)
     assert r == Rational(-2131, 2911)
     assert_almost_equal(x, float(r), places=7)
 
+    r = find_rational_approximation(x, method="farey", max_denominator=1000)
+    assert r == Rational(-571, 780)
+
+
+def test_rational_approximation_between_half_and_one():
+    x = math.sqrt(3) - 1  # 0.7320508075688772
+    r = find_rational_approximation(x, method="farey", places=7)
+    assert r == Rational(2131, 2911)
+    assert_almost_equal(x, float(r), places=7)
+
+    r = find_rational_approximation(x, method="farey", max_denominator=1000)
+    assert r == Rational(571, 780)
+
 
 def test_rational_approximation_greater_than_one():
-    x = math.sqrt(17)
-    r = find_rational_approximation(x, places=7)
+    x = math.sqrt(17)  # 4.123105625617661
+    r = find_rational_approximation(x, method="farey", places=7)
     assert r == Rational(17684, 4289)
     assert_almost_equal(x, float(r), places=7)
+
+    r = find_rational_approximation(x, method="farey", max_denominator=1000)
+    assert r == Rational(2177, 528)
+
+
+def test_rational_approximation_extremal():
+    x = 0.000000001
+    r = find_rational_approximation(x, method="farey", places=7)
+    assert r == Rational(0, 1)
+    assert_almost_equal(x, float(r), places=7)
+
+    r = find_rational_approximation(x, method="farey", max_denominator=1000)
+    assert r == Rational(0, 1)
+
+    x = 0.0009
+    r = find_rational_approximation(x, method="farey", max_denominator=1000)
+    assert r == Rational(1, 1000)
