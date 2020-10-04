c3487ac32ccef09b5db332dc5689bbf6b5116907
pdemp1729 <pdempster2@gmail.com>
2020-10-04 14:47:36 +0100

    Update README and setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as fh:
 
 setup(
     name="farey_algorithm",
-    version="0.1",
-    description="Implementation of Farey algorithm.",
+    version="0.2",
+    description="Implementation of best-rational-approximation algorithms.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pdemp1729/farey_algorithm",
     author="Paul Dempster",
     author_email="pdempster2@gmail.com",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
     ],
