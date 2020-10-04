3de7d177292d29bc2a9b51d8053e62a4ed71ff90
pdemp1729 <pdempster2@gmail.com>
2020-10-04 13:54:10 +0100

    Add algo for continued fraction method with input as accuracy
    - This algorithm is based on a similar idea to the previous one
    involving continued fractions, i.e. using semiconvergents, etc.

--- farey/tests/test_algo.py
+++ farey/tests/test_algo.py
@@ -2,7 +2,7 @@ import math
 
 from pytest import raises
 
-from farey.algo import find_rational_approximation
+from farey.algo import ALLOWED_METHODS, find_rational_approximation
 from farey.data import Rational
 from farey.tests import assert_almost_equal
 
@@ -15,7 +15,7 @@ def test_input():
 
     with raises(ValueError) as excinfo:
         _ = find_rational_approximation(x, method="continued_fraction")
-    assert str(excinfo.value) == "must specify max_denominator"
+    assert str(excinfo.value) == "must specify one of places or max_denominator"
 
     with raises(ValueError) as excinfo:
         _ = find_rational_approximation(x, method="my own")
@@ -26,109 +26,102 @@ def test_input():
 
 def test_rational_approximation_of_irrational():
     x = math.sqrt(2) - 1  # 0.41421356237309515
-    r = find_rational_approximation(x, method="farey", places=7)
-    assert r == Rational(2378, 5741)
-    assert_almost_equal(x, float(r), places=7)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, places=7)
+        assert r == Rational(2378, 5741)
+        assert_almost_equal(x, float(r), places=7)
 
-    rf = find_rational_approximation(x, method="farey", max_denominator=1000)
-    rc = find_rational_approximation(
-        x, method="continued_fraction", max_denominator=1000
-    )
-    assert rf == rc == Rational(408, 985)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, max_denominator=1000)
+        assert r == Rational(408, 985)
 
 
 def test_rational_approximation_of_rational():
     x = 0.5
-    r = find_rational_approximation(x, method="farey", places=7)
-    assert r == Rational(1, 2)
-    assert_almost_equal(x, float(r), places=7)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, places=7)
+        assert r == Rational(1, 2)
+        assert_almost_equal(x, float(r), places=7)
 
     x = 0.4
-    r = find_rational_approximation(x, method="farey", places=7)
-    assert r == Rational(2, 5)
-    assert_almost_equal(x, float(r), places=7)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, places=7)
+        assert r == Rational(2, 5)
+        assert_almost_equal(x, float(r), places=7)
 
     x = 0.5
-    rf = find_rational_approximation(x, method="farey", max_denominator=1000)
-    rc = find_rational_approximation(
-        x, method="continued_fraction", max_denominator=1000
-    )
-    assert rf == rc == Rational(1, 2)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, max_denominator=1000)
+        assert r == Rational(1, 2)
 
     x = 0.4
-    rf = find_rational_approximation(x, method="farey", max_denominator=1000)
-    rc = find_rational_approximation(
-        x, method="continued_fraction", max_denominator=1000
-    )
-    assert rf == rc == Rational(2, 5)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, max_denominator=1000)
+        assert r == Rational(2, 5)
 
 
 def test_rational_approximation_zero():
     x = 0
-    r = find_rational_approximation(x, method="farey", places=7)
-    assert r == Rational(0, 1)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, places=7)
+        assert r == Rational(0, 1)
 
-    r = find_rational_approximation(x, method="continued_fraction", max_denominator=10)
-    assert r == Rational(0, 1)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, max_denominator=10)
+        assert r == Rational(0, 1)
 
 
 def test_rational_approximation_less_than_zero():
     x = 1 - math.sqrt(3)  # -0.7320508075688772
-    r = find_rational_approximation(x, method="farey", places=7)
-    assert r == Rational(-2131, 2911)
-    assert_almost_equal(x, float(r), places=7)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, places=7)
+        assert r == Rational(-2131, 2911)
+        assert_almost_equal(x, float(r), places=7)
 
-    rf = find_rational_approximation(x, method="farey", max_denominator=1000)
-    rc = find_rational_approximation(
-        x, method="continued_fraction", max_denominator=1000
-    )
-    assert rf == rc == Rational(-571, 780)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, max_denominator=1000)
+        assert r == Rational(-571, 780)
 
 
 def test_rational_approximation_between_half_and_one():
     x = math.sqrt(3) - 1  # 0.7320508075688772
-    r = find_rational_approximation(x, method="farey", places=7)
-    assert r == Rational(2131, 2911)
-    assert_almost_equal(x, float(r), places=7)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, places=7)
+        assert r == Rational(2131, 2911)
+        assert_almost_equal(x, float(r), places=7)
 
-    rf = find_rational_approximation(x, method="farey", max_denominator=1000)
-    rc = find_rational_approximation(
-        x, method="continued_fraction", max_denominator=1000
-    )
-    assert rf == rc == Rational(571, 780)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, max_denominator=1000)
+        assert r == Rational(571, 780)
 
 
 def test_rational_approximation_greater_than_one():
     x = math.sqrt(17)  # 4.123105625617661
-    r = find_rational_approximation(x, method="farey", places=7)
-    assert r == Rational(17684, 4289)
-    assert_almost_equal(x, float(r), places=7)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, places=7)
+        assert r == Rational(17684, 4289)
+        assert_almost_equal(x, float(r), places=7)
 
-    rf = find_rational_approximation(x, method="farey", max_denominator=1000)
-    rc = find_rational_approximation(
-        x, method="continued_fraction", max_denominator=1000
-    )
-    assert rf == rc == Rational(2177, 528)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, max_denominator=1000)
+        assert r == Rational(2177, 528)
 
 
 def test_rational_approximation_extremal():
     x = 0.000000001
-    r = find_rational_approximation(x, method="farey", places=7)
-    assert r == Rational(0, 1)
-    assert_almost_equal(x, float(r), places=7)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, places=7)
+        assert r == Rational(0, 1)
+        assert_almost_equal(x, float(r), places=7)
 
-    rf = find_rational_approximation(x, method="farey", max_denominator=1000)
-    rc = find_rational_approximation(
-        x, method="continued_fraction", max_denominator=1000
-    )
-    assert rf == rc == Rational(0, 1)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, max_denominator=1000)
+        assert r == Rational(0, 1)
 
     x = 0.0009
-    rf = find_rational_approximation(x, method="farey", max_denominator=1000)
-    rc = find_rational_approximation(
-        x, method="continued_fraction", max_denominator=1000
-    )
-    assert rf == rc == Rational(1, 1000)
+    for method in ALLOWED_METHODS:
+        r = find_rational_approximation(x, method=method, max_denominator=1000)
+        assert r == Rational(1, 1000)
 
 
 def test_continued_fraction_method_full_coverage():
